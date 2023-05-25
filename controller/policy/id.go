c22f3977b1358a1654de270bfffad29f6bebcb35
Théo Bori <theo1.bori@epitech.eu>
2023-05-25 14:59:25 +0200

    [skip ci] feat: policy ID constraints abstraction

---
+++ controller/policy/id.go
@@ -0,0 +1,10 @@
+package policy
+
+const (
+	// Policy minimum ID (not included)
+	PolicyMinimumID = 0
+	// Policy maximum ID (not include)
+	PolicyMaximumID = 10000
+	// Federation policy minimum ID (not included)
+	FedPolicyMinimumID = 100000
+)
